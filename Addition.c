180f01c7b30b4d268a8022ab0e8e30c688a11022
rahulmahtolia <48041645+rahulmahtolia@users.noreply.github.com>
2020-10-01 14:46:46 +0530

    Added comments and improved the readability.

--- Addition.c
+++ Addition.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-void main()
-{ int a,b,c;
-printf("Enter two Numbers : ");
-scanf("%d %d",&a,&b);
-c=a+b;
-printf("The Sum is %d",c);
+#include<stdio.h> // Including the header file for handling input and output
+void main() // Main function
+{ 
+  int num1,num2,num3;  // declaring three variables of integer datatype
+  printf("Enter two Numbers : "); // Asking for the input from the user
+  scanf("%d %d",&num1,&num2); // storing the value in num1 and num2
+  num3=num1+num2; // performing the addition
+  printf("The sum is %d",num3); // Printing the sum
 }
